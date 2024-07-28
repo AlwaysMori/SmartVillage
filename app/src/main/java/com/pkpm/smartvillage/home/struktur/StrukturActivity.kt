ccc5f23fd5abbdcd8fff80af0fa5d89c441acfd8
AlwaysMori <116044642+AlwaysMori@users.noreply.github.com>
2024-07-28 10:29:59 +0700

    optimize

--- app/src/main/java/com/pkpm/smartvillage/home/struktur/StrukturActivity.kt
+++ app/src/main/java/com/pkpm/smartvillage/home/struktur/StrukturActivity.kt
@@ -1,46 +1,70 @@
 package com.pkpm.smartvillage.home.struktur
-import android.content.Intent
-import android.net.Uri
+
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.pkpm.smartvillage.R
-import com.pkpm.smartvillage.databinding.ActivityMainBinding
 import com.pkpm.smartvillage.databinding.ActivityStrukturBinding
 
 class StrukturActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityStrukturBinding
 
+    private lateinit var kadesAdapter: StrukturAdapter
+    private lateinit var sekdesAdapter: StrukturAdapter
+    private lateinit var kadusAdapter: StrukturAdapter
+
+    private val kadesList = mutableListOf<Struktur>()
+    private val sekdesList = mutableListOf<Struktur>()
+    private val kadusList = mutableListOf<Struktur>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityStrukturBinding.inflate(layoutInflater)
         setContentView(binding.root)
         enableEdgeToEdge()
+        kadesList.apply {
+            add(Struktur("Nama", "Jabatan", R.drawable.avatar, "628123456789"))
+        }
 
+        sekdesList.apply {
+            add(Struktur("Nama", "Jabatan", R.drawable.avatar, "628123456789"))
+        }
 
-        val phoneNumbers = arrayOf(
-            "+621234567890", "+621234567891", "+621234567892",
-            "+621234567893", "+621234567894", "+621234567895",
-            "+621234567896", "+621234567897", "+621234567898",
-            "+621234567899", "+621234567800", "+621234567801",
-            "+621234567802"
-        )
+        kadusList.apply {
 
-        val buttons = arrayOf(
-            binding.wa1, binding.wa2, binding.wa3, binding.wa4, binding.wa5,
-            binding.wa6, binding.wa7, binding.wa8, binding.wa9, binding.wa10,
-            binding.wa11, binding.wa12
-        )
+            add(Struktur("ahmad ", "Jabatan", R.drawable.avatar, "628987654321", "I"))
+            add(Struktur("jono ", "Jabatan", R.drawable.avatar, "628987654321", "II"))
+            add(Struktur("bima ", "Jabatan", R.drawable.avatar, "628987654321", "III"))
+            add(Struktur("sion ", "Jabatan", R.drawable.avatar, "628987654321", "IV"))
+            add(Struktur("lala ", "Jabatan", R.drawable.avatar, "628987654321", "V"))
+            add(Struktur("nanda ", "Jabatan", R.drawable.avatar, "628987654321", "VI"))
+            add(Struktur("soni ", "Jabatan", R.drawable.avatar, "628987654321", "VII"))
+            add(Struktur("bian ", "Jabatan", R.drawable.avatar, "628987654321", "VIII"))
+            add(Struktur("roni ", "Jabatan", R.drawable.avatar, "628987654321", "IX"))
+            add(Struktur("Iwan ", "Jabatan", R.drawable.avatar, "628987654321", "X"))
 
-        for (i in buttons.indices) {
-            buttons[i].setOnClickListener {
-                val intent = Intent(Intent.ACTION_VIEW).apply {
-                    data = Uri.parse("https://wa.me/${phoneNumbers[i]}")
-                }
-                startActivity(intent)
-            }
+        }
+
+        binding.kadesRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.sekdesRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.kadusRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        kadesAdapter = StrukturAdapter(kadesList, false)
+        sekdesAdapter = StrukturAdapter(sekdesList, false)
+        kadusAdapter = StrukturAdapter(kadusList, true)
+
+        binding.kadesRecyclerView.adapter = kadesAdapter
+        binding.sekdesRecyclerView.adapter = sekdesAdapter
+        binding.kadusRecyclerView.adapter = kadusAdapter
+
+        binding.edSearch.addTextChangedListener { text ->
+            val query = text.toString()
+            kadesAdapter.filter(query)
+            sekdesAdapter.filter(query)
+            kadusAdapter.filter(query)
         }
     }
 }
